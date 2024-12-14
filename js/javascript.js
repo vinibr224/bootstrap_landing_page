8635da75329ec78ae280dde98f7f603cd4a863fd
Peso_Alto <viniciusmosna95@gmail.com>
2024-12-14 00:51:48 -0300

    organizacao de comentarios

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,21 @@
 const botao_modo = document.getElementById("checkbox_input");
 
-// Adiciona o evento de mudança no checkbox
+// evento de mudança no checkbox
 botao_modo.addEventListener("change", () => {
     if (document.documentElement.getAttribute("data-bs-theme") === "light") {
         document.documentElement.setAttribute("data-bs-theme", "dark");
-        localStorage.setItem("modo_escuro", "true"); // Salva como string
+        localStorage.setItem("modo_escuro", "true"); // salva como string
     } else {
         document.documentElement.setAttribute("data-bs-theme", "light");
         localStorage.setItem("modo_escuro", "false");
     }
 });
 
-// Verifica o estado salvo no localStorage e ajusta o tema ao carregar a página
+// verifica o localStorage e ajusta o tema
 if (localStorage.getItem("modo_escuro") === "true") {
     document.documentElement.setAttribute("data-bs-theme", "dark");
-    botao_modo.checked = true; // Marca o checkbox
+    botao_modo.checked = true; // marca o checkbox
 } else {
     document.documentElement.setAttribute("data-bs-theme", "light");
-    botao_modo.checked = false; // Desmarca o checkbox
+    botao_modo.checked = false; // desmarca o checkbox
 }
